76e86e0702eb513ae6d0c21eb5a5bb1976aeb00e
Usagi Ito <usagi@WonderRabbitProject.net>
2013-02-04 23:01:12 +0900

    support shader program and bind GLSL variables with variadic templates.

--- include/WonderRabbitProject/GLEW.hpp
+++ include/WonderRabbitProject/GLEW.hpp
@@ -49,26 +49,35 @@ namespace WonderRabbitProject { namespace GLEW {
   #include "./GLEW/SHADER.hpp"
   
   struct glew;
+  struct program;
 
   template<GL::GLuint SHADER_TYPE>
   struct shader
   {
     friend glew;
+    friend program;
     static constexpr SHADER shader_type = SHADER(SHADER_TYPE);
     using this_type = shader<SHADER_TYPE>;
     
-    shader(const this_type&) = delete;
     shader(this_type&& o)
       : shader_(std::move(o.shader_))
-    { }
+    { o.cancel(); }
+    shader(const this_type&) = delete;
     
-    this_type& operator=(const this_type&) = delete;
     this_type& operator=(this_type&&)      = delete;
+    this_type& operator=(const this_type&) = delete;
 
-    void cancel()
-    { finalizer_ = []{}; }
+    inline void cancel()
+    {
+      L(INFO, "WRP::GLEW::shader<"
+        << to_string(shader_type)
+        << ">::calcel; this address is "
+        << std::hex << this
+      );
+      finalizer_ = []{};
+    }
     
-    void source(std::string&& v)
+    inline void source(std::string&& v)
     {
       L(INFO, "WRP::GLEW::shader<"
         << to_string(shader_type)
@@ -77,7 +86,7 @@ namespace WonderRabbitProject { namespace GLEW {
       source_ = std::move(v);
     }
 
-    void source(std::istream&& s)
+    inline void source(std::istream&& s)
     {
       L(INFO, "WRP::GLEW::shader<"
         << to_string(shader_type)
@@ -90,10 +99,10 @@ namespace WonderRabbitProject { namespace GLEW {
       s.read(const_cast<char*>(source_.data()), size);
     }
 
-    const std::string& source() const
+    inline const std::string& source() const
     { return source_; }
 
-    void compile()
+    inline void compile() const
     {
       L(INFO, "WRP::GLEW::shader<" << to_string(shader_type) << ">::compile");
       {
@@ -130,14 +139,16 @@ namespace WonderRabbitProject { namespace GLEW {
     ~shader()
     {
       L(INFO, "WRP::GLEW::shader<" << to_string(shader_type) << ">::dtor");
-      L(INFO, "shader object = " << std::hex << shader_);
       finalizer_();
-      L(INFO, "glDeleteShader done");
     }
   private:
     shader()
       : shader_(C::glCreateShader(GL::GLuint(shader_type)))
-      , finalizer_([this]{C::glDeleteShader(this->shader_);})
+      , finalizer_([this]{
+        L(INFO, "shader object = " << std::hex << this->shader_);
+        C::glDeleteShader(this->shader_);
+        L(INFO, "glDeleteShader done");
+      })
     {
       L(INFO, "WRP::GLEW::shader<" << to_string(shader_type) << ">::ctor");
       L(INFO, "shader object = " << std::hex << shader_);
@@ -153,6 +164,125 @@ namespace WonderRabbitProject { namespace GLEW {
   using geometry_shader        = shader<GL::GLuint(SHADER::GEOMETRY)>;
   using tess_control_shader    = shader<GL::GLuint(SHADER::TESS_CONTROL)>;
   using tess_evaluation_shader = shader<GL::GLuint(SHADER::TESS_EVALUATION)>;
+
+  struct program
+  {
+    friend glew;
+    
+    using this_type = program;
+
+    program(const this_type&) = delete;
+    program(this_type&& o)
+      : program_(std::move(o.program_))
+      , finalizer_(std::move(o.finalizer_))
+    { }
+
+    this_type& operator=(const this_type&) = delete;
+    this_type& operator=(this_type&&)      = delete;
+
+    inline void cancel()
+    { finalizer_ = []{}; }
+    
+    template<class T>
+    inline void attach(const T& shader_) const
+    {
+      L(INFO, "WRP::GLEW::program::attach; shader address is "
+        << std::hex << &shader_
+      );
+      C::glAttachShader(program_, shader_.shader_);
+    }
+    
+    template<class T, class ... TS>
+    inline void attach(const T& shader_, const TS& ... ts) const
+    {
+      attach(shader_);
+      attach(ts ...);
+    }
+
+    inline void bind_vs(const GL::GLuint index, const std::string& name)
+      const
+    {
+      L(INFO,
+        "WRP::GLEW::program::bind_vs; "
+        "[" << index << "]:" << name
+      );
+      C::glBindAttribLocation(program_, index, name.data());
+    }
+    
+    template<class ... TS>
+    inline void bind_vs(const std::string& name, const TS& ... ts)
+      const
+    {
+      bind_vs_(0, name, ts ...);
+    }
+    
+    inline void bind_fs(const GL::GLuint index, const std::string& name)
+      const
+    {
+      L(INFO,
+        "WRP::GLEW::program::bind_fs; "
+        "[" << index << "]:" << name
+      );
+      C::glBindFragDataLocation(program_, index, name.data());
+    }
+    
+    template<class ... TS>
+    inline void bind_fs(const std::string& name, const TS& ... ts)
+      const
+    {
+      bind_fs_(0, name, ts ...);
+    }
+    
+    inline void link() const
+    {
+      L(INFO, "WRP::GLEW::program::link");
+      
+      C::glLinkProgram(program_);
+      C::GLint result;
+      C::glGetProgramiv(program_, GL_LINK_STATUS, &result);
+      if( ! result )
+      {
+        auto message = "fail";
+        L(ERROR, message);
+        throw std::runtime_error(message);
+      }
+      L(INFO, "succeed");
+    }
+    
+  private:
+    program()
+      : finalizer_([this]{C::glDeleteProgram(this->program_);})
+    {
+      L(INFO, "WRP::GLEW::program::ctor");
+      L(INFO, "program object = " << std::hex << program_);
+    }
+    
+    inline void attach() const { }
+    
+    inline void bind_vs_(size_t) const {}
+    
+    template<class ... TS>
+    inline void bind_vs_(size_t index, const std::string& name, const TS& ... ts)
+      const
+    { 
+      bind_vs(index, name);
+      bind_vs_(index + 1, ts ...);
+    }
+    
+    inline void bind_fs_(size_t) const {}
+    
+    template<class ... TS>
+    inline void bind_fs_(size_t index, const std::string& name, const TS& ... ts)
+      const
+    { 
+      bind_fs(index, name);
+      bind_fs_(index + 1, ts ...);
+    }
+    
+    GL::GLuint program_;
+    std::function<void()> finalizer_;
+    
+  };
 
   struct glew final
   {
@@ -173,6 +303,18 @@ namespace WonderRabbitProject { namespace GLEW {
       return TSHADER();
     }
     
+    program create_program() const
+    {
+      L(INFO,  "--> WRP::GLEW::glew::create_program");
+      return program();
+    }
+
+    void use_program(const program& p) const
+    {
+      L(INFO, "--> WRP::GLEW::glew::use_program; program address is " << &p);
+      C::glUseProgram(p.program_);
+    }
+
     static this_type& instance()
     {
       L(INFO, "--> WRP::GLEW::glew::instance");
